ac8627c718662a152113814c66b267bd6614cfd2
Ewerton Gomes Lisboa <149094494+ewertongomeslisboa@users.noreply.github.com>
2025-10-23 23:16:40 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// Substitua 'nome-do-repositorio' pelo nome exato do seu repositório no GitHub
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+  base: 'https://github.com/ewertongomeslisboa/ecovibe.io.git',
+})
