f8cf993258611cf1eb2d653ed384c6f8bd7d47a0
Ewerton Gomes Lisboa <149094494+ewertongomeslisboa@users.noreply.github.com>
2025-10-23 23:25:12 -0300

    Update base URL in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // Substitua 'nome-do-repositorio' pelo nome exato do seu repositório no GitHub
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/ewertongomeslisboa/ecovibe.io.git',
+  base: 'https://github.com/ewertongomeslisboa/ecovibe.io',
 })
